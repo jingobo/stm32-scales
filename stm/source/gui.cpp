a9bda1648d47fdaa592284198c24a58523161ba5
Vitaly Alpatov <exclude@bk.ru>
2019-10-21 23:42:14 +0300

    ~ Зачатки модуля пользовательского интерфейса

--- stm/source/gui.cpp
+++ stm/source/gui.cpp
@@ -1,17 +1,59 @@
 ﻿#include "gui.h"
-#include "io.h"
+#include "list.h"
 #include "timer.h"
 
-static timer_callback_t led_timer([](void)
+// Класс базового визуального элемента
+class gui_visual_t : list_item_t
 {
-    IO_PORT_RESET(IO_LED);
-});
+    friend class gui_container_t;
+    // Флаг необходимости перерисовки
+    bool paint_needed;
+protected:
+    // Конструктор по умолчанию
+    gui_visual_t(void) : paint_needed(true)
+    { }
+
+    // Обработчик события вывода на экран
+    virtual void paint_handler(void) = 0;
+public:
+    // Событие смены состояния кнопки
+    virtual void key_event(key_kind_t key, key_state_t state)
+    { }
+    
+    // Событие вывод на экран
+    void paint_event(void)
+    {
+        if (!paint_needed)
+            return;
+        paint_handler();
+        paint_needed = true;
+    }
+    
+    // Обновление состояния элемента
+    virtual void refresh()
+    {
+        paint_needed = true;
+    }
+};
+
+// Класс базового контейнера
+class gui_container_t : public gui_visual_t
+{
+    // Список дочерних элементов
+    list_template_t<gui_visual_t> childs;
+    
+};
+
+class gui_overlay_t
+{
+    
+};
 
 void gui_init(void)
-{ }
+{ 
+}
 
 void gui_key_event(key_kind_t key, key_state_t state)
 {
-    IO_PORT_SET(IO_LED);
-    led_timer.start_us(10 * 1000);
+    
 }
