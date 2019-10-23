b233daf62add6b89d049270ccd5b3dada9710cb1
Vitaly Alpatov <exclude@bk.ru>
2019-10-23 23:08:57 +0300

    + Добавлены иходные файлы ресурсов

---
+++ stm/source/font.h
@@ -0,0 +1,9 @@
+﻿#ifndef __FONT_H
+#define __FONT_H
+
+#include "typedefs.h"
+
+// Шрифты
+extern const uint8_t FONT_HEADER_BOLD[];
+
+#endif // __FONT_H
---
+++ stm/source/image.h
@@ -0,0 +1,19 @@
+﻿#ifndef __IMAGE_H
+#define __IMAGE_H
+
+#include "typedefs.h"
+
+// Изображения
+extern const uint8_t IMAGE_BUTTON_ENTER[];
+extern const uint8_t IMAGE_BUTTON_UP[];
+extern const uint8_t IMAGE_BUTTON_DOWN[];
+extern const uint8_t IMAGE_BUTTON_BACK[];
+
+extern const uint8_t IMAGE_ROUND_LT[];
+extern const uint8_t IMAGE_ROUND_RT[];
+extern const uint8_t IMAGE_ROUND_RB[];
+extern const uint8_t IMAGE_ROUND_LB[];
+
+extern const uint8_t IMAGE_STATUS_ICON[];
+
+#endif // __IMAGE_H
