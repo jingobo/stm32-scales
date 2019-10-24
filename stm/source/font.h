c0a7e07b4cc9449815573b5603071bc061851baa
Vitaly Alpatov <exclude@bk.ru>
2019-10-24 22:51:02 +0300

    + Добавлены новые шрифты

--- stm/source/font.h
+++ stm/source/font.h
@@ -5,5 +5,10 @@
 
 // Шрифты
 extern const uint8_t FONT_HEADER_BOLD[];
+extern const uint8_t FONT_BUTTON_BOLD[];
+extern const uint8_t FONT_NUMBER_BOLD[];
+
+// Получает высоту шрифта
+uint8_t font_height_get(const uint8_t *font);
 
 #endif // __FONT_H
