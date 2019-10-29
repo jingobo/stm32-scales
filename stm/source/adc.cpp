72ff39901782007fe17c83b7f1f004375fee5f4b
Vitaly Alpatov <exclude@bk.ru>
2019-10-29 22:16:32 +0300

    ~ Переработка работы АЦП по прерыванию

--- stm/source/adc.cpp
+++ stm/source/adc.cpp
@@ -1,25 +1,63 @@
 ﻿#include "io.h"
 #include "adc.h"
 #include "mcu.h"
+#include "nvic.h"
+#include "event.h"
+#include "timer.h"
 
-// Размер скользящего окна усриднения (кратно 10)
-#define ADC_WINDOW_SIZE     50
+// Список обработчиков события АЦП
+adc_event_handler_t::list_t adc_event_list;
 
-// Локальные переменные модуля
-static __no_init struct
+// Значение АЦП в прерывании
+static __no_init uint32_t adc_value_irq;
+
+// Сторожевой таймер отказа АЦП
+static timer_notify_t adc_watchdog([](void)
 {
-    // Указатель на текущее записываемое значение
-    uint32_t pointer;
-    // Сумма всех элементов окна
-    int32_t summ;
-    // Скользящее окно усреднения
-    int32_t window[ADC_WINDOW_SIZE];
-} adc;
+    adc_event_list(ADC_VALUE_WATCHDOG);
+});
+
+// Рестарт сторожевого таймера АЦП
+static void adc_watchdog_reset(void)
+{
+    adc_watchdog.start_hz(2);
+}
 
 void adc_init(void)
 {
-    memset(&adc, 0, sizeof(adc));
+    EXTI->FTSR1 |= EXTI_FTSR1_FT4;                                              // EXTI interrupt on faiilng edge
+    EXTI->IMR1 |= EXTI_IMR1_IM4;                                                // EXTI interrupt unmask
+    SYSCFG->EXTICR[1] |= SYSCFG_EXTICR2_EXTI4_PB;                               // Select PB4 on EXTI line
+    // Вклшючение прерывания
+    nvic_irq_enable_set(EXTI4_IRQn, true);
+    // Запуск сторожевого таймера
+    adc_watchdog_reset();
+    // Запуск в ручную
+    if (!IO_PORT_READ(IO_HX_RX))
+        adc_interrupt_exti();
 }
+
+// Событие готовности значения
+static event_notify_t adc_value_ready([](void)
+{
+    // Забор значения
+    IRQ_CTX_SAVE();
+        IRQ_CTX_DISABLE();
+        union
+        {
+            uint32_t u32;
+            int32_t s32;
+        };
+        u32 = adc_value_irq;
+    IRQ_CTX_RESTORE();
+    // Конвертирование в 32-бит знаковое
+    if (u32 & 0x800000)
+        u32 |= 0xFF000000;
+    // Вызов события
+    adc_event_list(s32);
+    // Сброс сторожевого таймера
+    adc_watchdog_reset();
+});
 
 // Синхронная задержка для ногодрыга
 static void adc_delay(void)
@@ -28,65 +66,34 @@ static void adc_delay(void)
     mcu_delay(5);
 }
 
-// Внутрення функция чтения данных из АЦП (1 конвертирование)
-static void adc_conversion(void)
+IRQ_ROUTINE
+void adc_interrupt_exti(void)
 {
-    // Ожидание окончания преобразования
-    WAIT_WHILE(IO_PORT_READ(IO_HX_RX));
-    
-    // Запрет прерываний
-    IRQ_CTX_SAVE();
-        IRQ_CTX_DISABLE();
-        
-    	union
-        {
-            uint32_t u32;
-            int32_t s32;
-        } = { 0 };
-        
-        // Конвертирование
-        for (uint8_t i = 0; i < 24; i++)
-        {
-            // Поднимаем строб
-            IO_PORT_SET(IO_HX_CLK);
-            adc_delay();
-            // Сдвига значения АЦП
-            u32 <<= 1;
-            // Опускаем строб
-            IO_PORT_RESET(IO_HX_CLK);
-            adc_delay();
-            // Забираем очередной бит
-            if (IO_PORT_READ(IO_HX_RX))
-                u32++;
-        }
-
-        // Старт выборрки по каналу А с коэффициентом усиления 128
+    // Буфер для значения
+    register uint32_t raw = 0;
+    // Конвертирование
+    for (uint8_t i = 0; i < 24; i++)
+    {
+        // Поднимаем строб
         IO_PORT_SET(IO_HX_CLK);
-        adc_delay();
+            adc_delay();
+            raw <<= 1;
+        // Опускаем строб
         IO_PORT_RESET(IO_HX_CLK);
-    // Возвращяем прерывания
-	IRQ_CTX_RESTORE();
-    
-    // Конвертирование в 32-бит знаковое
-    if (u32 & 0x800000)
-        u32 |= 0xFF000000;
-        
-    // Обработка скользящего окна
-    adc.summ -= adc.window[adc.pointer];
-    adc.window[adc.pointer] = s32;
-    adc.summ += s32;
-    
-    // Смещение
-    if (++adc.pointer >= ADC_WINDOW_SIZE)
-        adc.pointer = 0;
-}
+        adc_delay();
+        // Забираем очередной бит
+        if (IO_PORT_READ(IO_HX_RX))
+            raw++;
+    }
 
-OPTIMIZE_SPEED
-int32_t adc_read(void)
-{
-    // Несколько конвертирований за раз
-    for (uint8_t i = 0; i < ADC_WINDOW_SIZE / 10; i++)
-         adc_conversion();
-    // Готово
-	return adc.summ / ADC_WINDOW_SIZE;
+    // Старт выборрки по каналу А с коэффициентом усиления 128
+    IO_PORT_SET(IO_HX_CLK);
+        adc_delay();
+    IO_PORT_RESET(IO_HX_CLK);
+
+    EXTI->PR1 |= IO_MASK(IO_HX_RX);                                             // Clear interrupt pending flag
+    
+    // Результат
+    adc_value_irq = raw;
+    adc_value_ready.raise();
 }
--- stm/source/adc.h
+++ stm/source/adc.h
@@ -1,11 +1,20 @@
 ﻿#ifndef __ADC_H
 #define __ADC_H
 
-#include "typedefs.h"
+#include "callback.h"
+
+// Значение АЦП при срабатывании сторожевого таймера
+#define ADC_VALUE_WATCHDOG      INT32_MIN
+
+// Тип обработчика события АЦП
+typedef callback_t<int32_t> adc_event_handler_t;
+
+// Список обработчиков события АЦП
+extern adc_event_handler_t::list_t adc_event_list;
 
 // Инициализация модуля
 void adc_init(void);
-// Чтение сырого 24-бит значения
-int32_t adc_read(void);
+// Обработчик прерывания по линии EXTI
+void adc_interrupt_exti(void);
 
 #endif // __ADC_H
